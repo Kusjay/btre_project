4ae335c7368c059a6c3371578a90191c05ed6fcf
Kusjay <kelechiuma@gmail.com>
2019-01-18 11:31:04 +0100

    Added requirements file

--- btre/settings.py
+++ btre/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -148,3 +149,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kelechiuma@gmail.com'
 EMAIL_HOST_PASSWORD = 'Kusjaypassword1.'
 EMAIL_USE_TLS = True
+
